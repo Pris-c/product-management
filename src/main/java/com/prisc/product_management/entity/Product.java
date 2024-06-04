3b14a382a11ff1134d6fd2ff2fb0a4d82552e0e8
Priscila <priscila.campoos@yahoo.com.br>
2024-06-04 16:09:49 +0100

    add: Lombok dependency

--- src/main/java/com/prisc/product_management/entity/Product.java
+++ src/main/java/com/prisc/product_management/entity/Product.java
@@ -1,10 +1,14 @@
 package com.prisc.product_management.entity;
 
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.UUID;
 
 @Entity
+@Getter
+@Setter
 public class Product {
 
     private UUID productId;
