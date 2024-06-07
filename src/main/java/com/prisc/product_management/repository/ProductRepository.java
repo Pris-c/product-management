db4b20c37f6cdb098231f02d21dc6cc37831d0b3
Priscila <priscila.campoos@yahoo.com.br>
2024-06-07 11:51:53 +0100

    update: ProductRepository

--- src/main/java/com/prisc/product_management/repository/ProductRepository.java
+++ src/main/java/com/prisc/product_management/repository/ProductRepository.java
@@ -2,8 +2,10 @@ package com.prisc.product_management.repository;
 
 import com.prisc.product_management.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
 }
